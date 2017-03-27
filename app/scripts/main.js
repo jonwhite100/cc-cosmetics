da746fd65adc5c0645d262690d77d2c2f3cea1b4
jonwhite100 <jon@bpm-web-design.co.uk>
2017-03-27 22:40:09 +0100

    one page set up completed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+(function($) {
+    'use strict'; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+})(jQuery); // End of use strict
